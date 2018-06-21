19d0a7be8cf2b391c17c0eb0ada434bae02775cc
James Black <james.black.jb2@roche.com>
2018-06-21 19:35:23 +0200

    start app

---
+++ app.R
@@ -0,0 +1,90 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(mongolite)
+
+## Database functions ------------
+
+
+  options(mongodb = list(
+    "host" = "ds163700.mlab.com:63700",
+    "username" = "shiny",
+    "password" = readLines("password")
+  ))
+  databaseName <- "rinpharma"
+  collectionName <- "responses"
+
+  saveMongoData <- function(data) {
+    # Connect to the database
+    db <- mongo(collection = collectionName,
+                url = sprintf(
+                  "mongodb://%s:%s@%s/%s",
+                  options()$mongodb$username,
+                  options()$mongodb$password,
+                  options()$mongodb$host,
+                  databaseName))
+    # Insert the data into the mongo collection as a data.frame
+    data <- as.data.frame(t(data))
+    db$insert(data)
+  }
+
+  loadMongoData <- function() {
+    # Connect to the database
+    db <- mongo(collection = collectionName,
+                url = sprintf(
+                  "mongodb://%s:%s@%s/%s",
+                  options()$mongodb$username,
+                  options()$mongodb$password,
+                  options()$mongodb$host,
+                  databaseName))
+    # Read all the entries
+    data <- db$find()
+    data
+  }
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+   # Application title
+   titlePanel("Old Faithful Geyser Data"),
+
+   # Sidebar with a slider input for number of bins
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2]
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
+
