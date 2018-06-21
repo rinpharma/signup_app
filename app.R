ec493e99614e66a3f0de71aa9ca7923a11e8f2cb
James Black <james.black.jb2@roche.com>
2018-06-21 22:58:26 +0200

    first draft

--- app.R
+++ app.R
@@ -8,81 +8,158 @@
 #
 
 library(shiny)
-library(mongolite)
+library(tidyverse)
+library(googlesheets)
+
+# gs_auth()
+
+## Database connect ------
 
 ## Database functions ------------
 
+table <- "RinPharmaRegistered"
 
-  options(mongodb = list(
-    "host" = "ds163700.mlab.com:63700",
-    "username" = "shiny",
-    "password" = readLines("password")
-  ))
-  databaseName <- "rinpharma"
-  collectionName <- "responses"
+saveData <- function(data) {
+  # Grab the Google Sheet
+  sheet <- gs_title(table)
+  # Add the data as a new row
+  gs_add_row(sheet, input = data)
+}
 
-  saveMongoData <- function(data) {
-    # Connect to the database
-    db <- mongo(collection = collectionName,
-                url = sprintf(
-                  "mongodb://%s:%s@%s/%s",
-                  options()$mongodb$username,
-                  options()$mongodb$password,
-                  options()$mongodb$host,
-                  databaseName))
-    # Insert the data into the mongo collection as a data.frame
-    data <- as.data.frame(t(data))
-    db$insert(data)
-  }
+loadData <- function() {
+  # Grab the Google Sheet
+  sheet <- gs_title(table)
+  # Read the data
+  gs_read_csv(sheet)
+}
 
-  loadMongoData <- function() {
-    # Connect to the database
-    db <- mongo(collection = collectionName,
-                url = sprintf(
-                  "mongodb://%s:%s@%s/%s",
-                  options()$mongodb$username,
-                  options()$mongodb$password,
-                  options()$mongodb$host,
-                  databaseName))
-    # Read all the entries
-    data <- db$find()
-    data
-  }
+### Load invites
+
+invited <- read_csv(
+    "invites.csv",
+    col_types = cols(
+      Type = col_character(),
+      Name = col_character(),
+      Email = col_character(),
+      Company = col_character()
+    )
+  ) %>%
+  mutate(
+    Email = tolower(Email)
+  )
+
+## Form maintenance -------
+
+  # Define the fields we want to save from the form
+  fields <- c("entered_email", "industry", "confirmed")
+
+### UI ----------
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("R/Pharma registration"),
 
-   # Sidebar with a slider input for number of bins
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
+   # Their email
+   textInput("entered_email", "Please enter your email address", ""),
 
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+   # Industry
+   radioButtons(
+     "industry",
+     "Please choose the research type that best represents you:",
+      c("Regulatory body",
+        "Pharma company",
+        "Contract Research Organisation",
+        "Non-profit",
+        "Academic",
+        "Student")),
+   helpText("Spaces are very limited, so please only register if you plan to attend"),
+   checkboxInput(
+     "confirmed",
+     "I confirm that I plan to attend R/Pharma on August 15th and 16th",
+     FALSE),
+   actionButton("submit", "Submit"),
+
+   hr(),
+
+   textOutput("response")
+
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2]
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  # Whenever a form is submitted aggregate all form data
+  formData <- eventReactive(input$submit,{
 
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+    withProgress(message = 'Processing registration', value = 0, {
+
+    incProgress(0.2, detail = paste("Running input checks"))
+
+    # check there is an email @
+    validate(
+      need(grep("@",input$entered_email) == 1,
+           "Please enter an email address")
+    )
+
+    # check if invited
+      validate(
+        need(tolower(input$entered_email) %in% tolower(invited$Email),
+             "Sorry, your email is not on the list of invited people")
+      )
+
+    # check they agree
+      validate(
+        need(input$confirmed,
+             "Unfortunately, we can only confirm tickets for people that can confirm they will attend")
+      )
+
+      incProgress(0.4, detail = paste("Checking if you are already registered"))
+
+    # download current sheet
+      old_data <- loadData()
+
+    # check if they are already registered
+    validate(
+      need(!tolower(input$entered_email) %in% tolower(old_data$Email),
+           paste(input$entered_email,"is already registered."))
+    )
+
+    # Merge with data
+      data <- data.frame(
+        Email = tolower(input$entered_email),
+        Industry = input$industry,
+        Attending = input$confirmed,
+        Time = Sys.time(),
+        stringsAsFactors = FALSE
+      ) %>%
+      left_join(
+        invited,
+        by = "Email"
+      ) %>%
+      select(
+        Name,Email,Time,Type,Industry,Company,Attending
+      )
+    })
+    data
+  })
+
+  # When the Submit button is clicked, save the form data
+  observeEvent(input$submit, {
+    tosave <- formData()
+    withProgress(message = 'Saving registration', value = 1, {
+      saveData(tosave)
+    })
+  })
+
+  output$response <- renderText({
+    input$submit
+    output <- formData()
+    paste0(
+      output$Name,", thank you for registering."
+    )
+  })
 }
 
 # Run the application
